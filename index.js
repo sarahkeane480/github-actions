f2a09e252659588c08b40699ac5616540becd18a
Replit user <>
2023-08-17 15:48:36 +0000

    Add calculator tests

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+var Calculator = require("./Calculator.js");//.js is optional
+var calc = new Calculator();
+let result = calc.subtract(2, 4);
+console.log(result);
